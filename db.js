7192edc21eb4f1ace16bff8558570ccc78d88f5f
durgeshkarunik <durgeshkarunik@gmail.com>
2024-07-23 19:43:53 +0530

    shift mongo db to online server

--- db.js
+++ db.js
@@ -1,25 +1,27 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+require('dotenv').config();
 // define the MongoDB connection URL
-const mongoURL ='mongodb://localhost:27017/hotels'
+// const mongoURL =process.env.MONGODB_URL_LOCAL
+
+const mongoURL =process.env.MONGODB_URL
 
 // Set up MongoDB connection
-mongoose.connect(mongoURL,{
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
-})
+mongoose.connect(mongoURL, {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true
+});
 const db = mongoose.connection;
 
 // Define event listeners for database connection
 
-db.on('connected',()=>{
-    console.log('Connected to MongoDB server')
-})
-db.on('error',(err)=>{
-    console.log('MongoDB connection error',err)
-})
-db.on('disconnected',()=>{
-    console.log('disConnected to MongoDB server')
-})
+db.on("connected", () => {
+  console.log("Connected to MongoDB server");
+});
+db.on("error", (err) => {
+  console.log("MongoDB connection error", err);
+});
+db.on("disconnected", () => {
+  console.log("disConnected to MongoDB server");
+});
 //export the database connection
-module.exports =db;
+module.exports = db;
--- servers.js
+++ servers.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.send(
@@ -17,6 +19,8 @@ app.use("/person", personRoutes);
 
 const menuItemRoutes = require("./routes/menuItemRoutes");
 app.use("/menu", menuItemRoutes);
-app.listen(3000, () => {
+
+
+app.listen(PORT, () => {
   console.log("listening on port 3000");
 });
